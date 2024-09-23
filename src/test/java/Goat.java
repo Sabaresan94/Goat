7cc0016303c8c723e417a13375c4e9c500331d58
Sabaresan94 <sabaresan94@gmail.com>
2024-09-23 10:02:49 +0530

    casting

---
+++ src/test/java/Goat.java
@@ -0,0 +1,16 @@
+
+public class Goat {
+	private void actor() {
+		System.out.println("actor is thalapathy");
+
+	}
+	private void actress() {
+		System.out.println("singapoor sallon heroin");
+	}
+	public static void main(String[] args) {
+		Goat g=new Goat();
+		g.actor();
+		g.actress();
+	}
+
+}
