5c4cebbeb83f4497c0925a065eb115bb791d12bf
Sabaresan94 <sabaresan94@gmail.com>
2024-09-23 12:15:09 +0530

    add

--- src/test/java/Goat.java
+++ src/test/java/Goat.java
@@ -7,6 +7,12 @@ public class Goat {
 	private void actress() {
 		System.out.println("singapoor sallon heroin");
 	}
+	private void director() {
+		System.out.println("vp");
+	}
+	private void music() {
+		System.out.println("u1");
+	}
 	public static void main(String[] args) {
 		Goat g=new Goat();
 		g.actor();
